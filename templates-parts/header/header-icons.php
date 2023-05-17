42381eb490b12c41e51a14b7e2bfc73860627483
Creastv <piotr@roial.pl>
2023-05-17 12:14:47 +0200

    added target link

--- templates-parts/header/header-icons.php
+++ templates-parts/header/header-icons.php
@@ -4,8 +4,9 @@ $icons  = get_field( 'ikony', 'options' );
 <?php if($icons) :
     echo '<ul class="navbar__icons">';
     foreach($icons as $s):
+       $target = $s['target'] == '_blank' ? 'target="_blank"' : false; 
         echo '<li>';
-           echo $s['link'] ? '<a href="' . $s['link'] . '" >' : false;
+           echo $s['link'] ? '<a href="' . $s['link'] . '" ' . $target . '  >' : false;
         //    echo $s['ikona'] ;
            echo '<img src="' .get_stylesheet_directory_uri() . '/src/img/icons/' . $s['ikona'] . '.svg"   height="21px" >';
            echo $s['link'] ? '</a>' : false;
