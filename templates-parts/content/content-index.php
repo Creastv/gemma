e25c1f5d8da7199a146ee498b66f03d91f4d2ff2
Creastv <piotr@roial.pl>
2023-05-22 09:47:33 +0200

    thumbnail

--- templates-parts/content/content-index.php
+++ templates-parts/content/content-index.php
@@ -1,9 +1,14 @@
+<?php
+$zf = get_field( 'zdjecie' );
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
     <div class="post-item__wraper">
         <header>
             <a href="<?php the_permalink(); ?>">
-                <?php if ( has_post_thumbnail() )  : ?>
-                <?php the_post_thumbnail('post-item'); ?>
+                <?php if (  $zf  )  : ?>
+                    <?php echo wp_get_attachment_image( $zf, 'post-item' );?>
+                <?php elseif ( has_post_thumbnail()  )  : ?>
+                    <?php echo the_post_thumbnail('post-item');?>
                 <?php else: ?>
                 <img src="<?php echo get_template_directory_uri()."/src/img/thumbnail.png"; ?>" width="350" height="490" alt="<?php the_title(); ?>">
                 <?php endif; ?>
