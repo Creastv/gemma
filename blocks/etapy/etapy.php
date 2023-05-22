72a780009794d38c0818f5ac3b7c1c5b73cde175
Creastv <piotr@roial.pl>
2023-05-22 15:00:49 +0200

    pod klucz

--- blocks/etapy/etapy.php
+++ blocks/etapy/etapy.php
@@ -1,3 +1,71 @@
 <?php
 $etapy = get_field( 'etapy' );
 ?>
+
+<div class="etapy">
+    <ul>
+        <li class="active">
+            <div class="etap">
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Dokumentacja<br>budowy</p>
+                </div>
+                <div class="etap__space"></div>
+            </div>
+        </li>
+        <li class="active">
+            <div class="etap">
+                <div class="etap__space"></div>
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Rozpoczęcie<br>prac budowlanych</p>
+                </div>
+            </div>
+        </li>
+        <li class="active">
+            <div class="etap">
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Stan surowy<br>otwarty</p>
+                </div>
+                <div class="etap__space"></div>
+            </div>
+        </li>
+        <li>
+            <div class="etap">
+                 <div class="etap__space"></div>
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Stan surowy<br>otwarty</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="etap">
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     p>Rozpoczęcie<br>prac budowlanych</p>
+                </div>
+                <div class="etap__space"></div>
+            </div>
+        </li>
+        <li>
+            <div class="etap">
+                 <div class="etap__space"></div>
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Stan surowy<br>otwarty</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="etap">
+                <div class="etap__content">
+                    <span class="pointer"></span>
+                     <p>Etap 1</p>
+                </div>
+                <div class="etap__space"></div>
+            </div>
+        </li>
+    </ul>
+</div>
