0ec428363d2bef264b4da97b82cb39e48dd57501
Creastv <piotr@roial.pl>
2023-05-24 18:19:54 +0200

    etapy

--- blocks/kroki/kroki.js
+++ blocks/kroki/kroki.js
@@ -1,6 +1,6 @@
 const li = document.querySelectorAll('.steps__content li')
 console.log(li)
-li[0].classList.add('active')
+// li[0].classList.add('active')
 
 for(i=0; i< li.length; i++){
     li[i].addEventListener('click', function(e){
