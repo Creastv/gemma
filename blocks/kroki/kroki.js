e5209892bbe3010b3ccb2b8135d20f67a0979571
Creastv <piotr@roial.pl>
2023-06-20 15:22:20 +0200

    post order

--- blocks/kroki/kroki.js
+++ blocks/kroki/kroki.js
@@ -1,13 +1,12 @@
-const li = document.querySelectorAll('.steps__content li')
-console.log(li)
-// li[0].classList.add('active')
+const li = document.querySelectorAll(".steps__content li");
+console.log(li);
+li[0].classList.add("active");
 
-for(i=0; i< li.length; i++){
-    li[i].addEventListener('click', function(e){
-        for(i=0; i<li.length; i++){
-            li[i].classList.remove('active')
-        }
-        e.target.parentElement.classList.add('active');
-    });
-    
+for (i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", function (e) {
+    for (i = 0; i < li.length; i++) {
+      li[i].classList.remove("active");
+    }
+    e.target.parentElement.classList.add("active");
+  });
 }
