e42dc39a70929e4a6e269cd8727fbb490380ed87
Creastv <piotr@roial.pl>
2023-08-07 10:12:51 +0200

    slider

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".s-slider", {
-   autoplay: {
-        delay: 4500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
--- src/js/go-cookies.js
+++ src/js/go-cookies.js
@@ -1,8 +1,12 @@
-document.querySelector(".results-display-table").addEventListener("click", function () {
-  document.cookie = "resultsDisplay=table; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-  location.reload();
-});
-document.querySelector(".results-display-grid").addEventListener("click", function () {
-  document.cookie = "resultsDisplay=; expires=Fri, 31 Dec 1970 23:59:59 GMT; path=/";
-  location.reload();
-});
+if (document.querySelector(".results-display-table") !== null) {
+  document.querySelector(".results-display-table").addEventListener("click", function () {
+    document.cookie = "resultsDisplay=table; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    location.reload();
+  });
+}
+if (document.querySelector(".results-display-grid") !== null) {
+  document.querySelector(".results-display-grid").addEventListener("click", function () {
+    document.cookie = "resultsDisplay=; expires=Fri, 31 Dec 1970 23:59:59 GMT; path=/";
+    location.reload();
+  });
+}
