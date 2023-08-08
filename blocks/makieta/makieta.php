bcbeed5b9b9d8435da0399da9a86c96b57e28819
Creastv <piotr@roial.pl>
2023-08-08 11:34:11 +0200

    search page

--- blocks/makieta/makieta.php
+++ blocks/makieta/makieta.php
@@ -10,6 +10,7 @@ $fill_color = "00000";
 $fill_opacity = "0.4";
 $stroke_opacity = "0.0";
 
+
 ?>
 
 <div class="go-makieta" data-inw="<?php echo esc_html( $term->slug); ?>" data-filter="<?php echo $filter; ?>" data-idform="<?php echo $form; ?>">
@@ -24,15 +25,40 @@ $stroke_opacity = "0.0";
            if(isset($cord['link'])) {
             $cordsLink = $cord['link'] ;
            }
+
+           $idPost = $cord['mieszkaniedomlokal'];
+           $idStatus = get_field( 'status', $idPost);
+           $status = '';
+           switch ($idStatus) {
+                case '1':
+                    $status = 'Wolne';
+                    $stroke_color = "008000";
+                    $fill_color = "008000";
+                    $fill_opacity = "0.5";
+                    $stroke_opacity = "0.5";
+                    break;
+                case '2':
+                    $status = 'Zarezerwowane';
+                    $stroke_color = "1788c9";
+                    $fill_color = "1788c9";
+                    $fill_opacity = "0.5";
+                    $stroke_opacity = "0.5";
+                    break;
+                case '3':
+                    $status = 'Sprzedane';
+                    break;
+            }
         ?>
         <area
+        data-status="<?php echo $status ?>"
         title="<?php echo $cord['naglowek']; ?>"
         date-na="<?php echo $cord['opis']; ?>"
         data-valu="<?php echo $cord['value']; ?>"
+   
         class=" hasTooltip "
         coords="<?php echo $cordsNumber; ?>"
         <?php echo $cordsLink ? $cordsLink : ''; ?>
-        data-maphilight='{"strokeOpacity ": "<?php echo $stroke_opacity; ?>", "strokeColor":"<?php echo $stroke_color; ?>","strokeWidth":1,"fillColor":"<?php echo $fill_color; ?>","fillOpacity":<?php echo $fill_opacity; ?>}'
+        data-maphilight='{"alwaysOn":true, "strokeOpacity ": "<?php echo $stroke_opacity; ?>", "strokeColor":"<?php echo $stroke_color; ?>","stroke":false,"strokeWidth":0,"fillColor":"<?php echo $fill_color; ?>","fillOpacity":<?php echo $fill_opacity; ?>}'
         shape="poly">
         <?php endforeach; ?>
     </map>
