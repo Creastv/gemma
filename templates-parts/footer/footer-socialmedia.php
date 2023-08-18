918b818281db73d9f5a8ceff5cd0f3b268a7b871
Creastv <piotr@roial.pl>
2023-08-18 15:32:04 +0200

    slider

--- templates-parts/footer/footer-socialmedia.php
+++ templates-parts/footer/footer-socialmedia.php
@@ -7,7 +7,7 @@ $sm = $lewo['social_media'];
     echo '<ul class="footer__socialmedia">';
     foreach($sm as $s):
         echo '<li>';
-           echo $s['link'] ? '<a href="' . $s['link'] . '" >' : false;
+           echo $s['link'] ? '<a href="' . $s['link'] . '" target="_blank" >' : false;
         //    echo $s['ikona'] ;
            echo '<img src="' .get_stylesheet_directory_uri() . '/src/img/icons/' . $s['ikona'] . '.svg"   height="28px" />';
            echo $s['link'] ? '</a>' : false;
