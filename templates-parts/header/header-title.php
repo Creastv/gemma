b4c6aad0ed7ccc56d1b3f63b14d5b53ef10a2f7f
Creastv <piotr@roial.pl>
2023-08-31 15:38:22 +0200

    ajax

--- templates-parts/header/header-title.php
+++ templates-parts/header/header-title.php
@@ -36,7 +36,7 @@ $display = get_field( 'wylacz_tytul_strony' );
 					elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
 						_e( 'Links', 'go' );
 					else :
-						_e( 'Blog', 'go' );
+						_e( 'Aktualności', 'go' );
 				endif; ?>
     </h1>
     <?php the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
